a3392a62145093c7018c99b303eaf0a1abe49660
aluno <aluno@PASAMESL04019.estacio.acad>
2017-10-20 18:32:33 -0300

    Interface do projeto

---
+++ src/venda/pkg1/pkg0/FXMLDocumentController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package venda.pkg1.pkg0;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+/**
+ *
+ * @author aluno
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Button jtgerar;
+
+    @FXML
+    private ListView<?> lvgerar;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    public void gerar(ActionEvent event){
+        
+    }
+}
